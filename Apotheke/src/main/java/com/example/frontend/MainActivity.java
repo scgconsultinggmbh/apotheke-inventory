c275cc5cafd54e8625fe60dd117872e13daf1b81
asish012 <asish012@gmail.com>
2020-05-26 09:23:09 +0200

    Initial Commit

---
+++ Apotheke/src/main/java/com/example/frontend/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Camera;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+//        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+//        mPreview = new CameraPreview(this, mCamera);
+//        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+//        preview.addView(mPreview);
+    }
+
+//    public static Camera getCameraInstance(){
+//        Camera c = null;
+//        try {
+//            c = Camera.open(); // attempt to get a Camera instance
+//        }
+//        catch (Exception e){
+//            Log.d(TAG, "----- Error opening camera: " + e.getMessage());
+//        }
+//        return c; // returns null if camera is unavailable
+//    }
+//
+//    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+//
+//        @Override
+//        public void onPictureTaken(byte[] data, Camera camera) {
+//
+//            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+//            if (pictureFile == null){
+//                Log.d(TAG, "Error creating media file, check storage permissions");
+//                return;
+//            }
+//
+//            try {
+//                FileOutputStream fos = new FileOutputStream(pictureFile);
+//                fos.write(data);
+//                fos.close();
+//            } catch (FileNotFoundException e) {
+//                Log.d(TAG, "File not found: " + e.getMessage());
+//            } catch (IOException e) {
+//                Log.d(TAG, "Error accessing file: " + e.getMessage());
+//            }
+//        }
+//    };
+}
